7d87f949b301fc1f5cd98c49937c7cb4a7d9b344
vvmruder <vvmruder@users.noreply.github.com>
2023-02-25 14:50:25 +0100

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,6 @@
 # Try to fetch if there are differences
 git fetch -p
+# bool if remote changes
 if [ $(git diff --name-only origin/master | wc -l) -eq "0" ]; then
   echo "  🟢 Git repo is clean."
 else
