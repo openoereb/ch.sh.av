33c6bd6a390234075a27fc4adb8525a31fe1b41e
Clemens Rudert <crud3_rt@posteo.ch>
2023-02-28 18:42:09 +0100

    set correct env

--- scripts/insert_data.sh
+++ scripts/insert_data.sh
@@ -2,7 +2,6 @@
 
 ili2pg_executable=$(find /ili2pg -iname 'ili2pg*.jar')
 av_model="DM01AVCH24LV95D"
-database="oereb"
 schema="live"
 topics="DM01AVCH24LV95D.Liegenschaften;DM01AVCH24LV95D.Gemeindegrenzen;DM01AVCH24LV95D.Gebaeudeadressen"
 
@@ -11,9 +10,9 @@ git clone https://github.com/openoereb/ch.sh.av.git /data
 for f in /data/*.itf; do
   java -jar "$ili2pg_executable" \
     --import \
-    --dbdatabase "$database" \
+    --dbdatabase "$POSTGRES_DB" \
     --dbusr postgres \
-    --dbpwd mysecretpassword \
+    --dbpwd "$POSTGRES_PASSWORD" \
     --dbschema "$schema" \
     --models "$av_model" \
     --topics "$topics" \
